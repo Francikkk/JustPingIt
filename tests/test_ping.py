f4d01ac6a146c3c0b34cdd04bc21bde29a5e0040
Francikkk <101191450+Francikkk@users.noreply.github.com>
2025-07-06 18:21:58 +0200

    adding tests

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -10,21 +10,21 @@ def test_ping_initialization() -> None:
     ping = Ping(result, ip)
 
     # Controlla che gli attributi siano correttamente assegnati
-    assert ping.result == result  # noqa: S201
-    assert ping.ip_address == ip  # noqa: S201
+    assert ping.result == result
+    assert ping.ip_address == ip
 
     # Controlla che il timestamp sia una stringa e corrisponda al formato:
     # "YYYY-MM-DD HH:MM:SS"
-    assert isinstance(ping.timestamp, str)  # noqa: S201
+    assert isinstance(ping.timestamp, str)
 
     # Usa regex per verificare il formato del timestamp
     pattern = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}"
-    assert re.match(pattern, ping.timestamp)  # noqa: S201
+    assert re.match(pattern, ping.timestamp)
 
     # (Opzionale) Verifica che il timestamp sia vicino al tempo corrente
     timestamp_datetime = datetime.strptime(ping.timestamp, "%Y-%m-%d %H:%M:%S")
     now = datetime.now()
     diff = (now - timestamp_datetime).total_seconds()
-    assert (  # noqa: S201
+    assert (
         diff >= 0 and diff < 5
     )  # Il test dovrebbe essere eseguito entro 5 secondi dalla creazione
